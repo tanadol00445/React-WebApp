2855999670c00737e6bb406380f5a90cd636c4ea
tanadol00445 <68319010004@skntc.ac.th>
2025-08-20 18:18:07 +0700

    20/08/2568

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+        <footer>
+            &#169; {`${new Date().getFullYear()} - React Developer`}    
+        </footer>
+    </>
+  )
+}
+
+export default Footer
